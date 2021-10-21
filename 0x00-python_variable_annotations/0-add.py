8a069705af196e5464c96d4c54dd77b2dcfebc15
Andy Lopez <profesor.de.ingles@aol.com>
2021-10-20 22:38:24 -0500

    |- UPDATED -| file

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -3,16 +3,5 @@
 
 
 def add(a: float, b: float) -> float:
-    """[type-annotated add function]
-
-    Args:
-        a ([int]): [dessscription]
-        b ([float]): [dsescription]
-
-    Returns:
-        [float]: [result of the addition]
-    """
+    """Adds two float numbers"""
     return a + b
-def fd():
-    return a + b
-    
