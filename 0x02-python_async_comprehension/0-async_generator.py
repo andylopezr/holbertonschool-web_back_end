4e9a696d14417f34180eab8b8a15dc5ca71996ed
Andy Lopez <profesor.de.ingles@aol.com>
2021-10-20 22:48:24 -0500

    |- CHANGED -| logic

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python3
-""" asynchronous generator """
-
-
-import random
+""" Module for storing the async_generator coroutine. """
 import asyncio
+from random import uniform
 from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """[summary]
-    async function generator 
-    Yields:
-        [Generator]: [iterable object of floats]
-    """
+    """ Waits for a second and yields a random number. """
     for _ in range(10):
-        x = random.uniform(0, 10)
         await asyncio.sleep(1)
-        yield x
+        yield uniform(0, 10)
